8173b6c8111ca8d318d7cc4ce49f78d89166d4a2
Grant Adams <adamsgd@uw.edu>
2020-10-16 08:44:49 -0700

    Update Truncated_von_Bertalanffy_growth_TMB.R

--- R/Truncated_von_Bertalanffy_growth_TMB.R
+++ R/Truncated_von_Bertalanffy_growth_TMB.R
@@ -1,7 +1,33 @@
-# This code creates a 3-parameter von Bertalanffy growth function assuming a left and right truncated lognormal distribution in template model builder.
+# This code creates a 3-parameter von Bertalanffy growth function assuming a left and right truncated normal distribution of log length in template model builder.
 
-# Load packages
-library(TMB)
+# NOTE: You need RTools to make TMB function!!!!
+# Follow the online instructions here:
+# https://github.com/kaskr/adcomp
+
+###### Simulate data
+N_obs = 1000 # Number of observations
+Min_age = 0 # Minimum age in the population
+Max_age = 10 # Maximum age in the population
+min_size = 40 # Minimum size harvested by the fishery
+max_size = 400 # Maximum size harvested by the fishery
+
+
+# Parameters
+Obs_sd = 20 # Standard deviation
+Linf = 350 # Asymptoptic length/mean length at maximum age (mm)
+k = .3 # Growth rate (yr^-1)
+t0 = -1.5 # Estimated age at length 0 (yr)
+
+# Simulation
+Age_i = runif(N_obs, Min_age, Max_age)
+Obs_i = rnorm(N_obs, Linf * (1 - exp(-k * (Age_i - t0))), Obs_sd)
+
+# Truncate data
+Age_i = Age_i[which(Obs_i >= min_size & Obs_i <= max_size)]
+Obs_i = Obs_i[which(Obs_i >= min_size & Obs_i <= max_size)]
+
+# Plot length-at-age
+plot(Age_i,Obs_i , xlab = "Age (yr)", ylab = "Length (mm)")
 
 #..........................................................................
 # Write model
@@ -71,3 +97,14 @@ write(tmb_model, file = "vbgf_trunc.cpp")
 # Load model template
 compile("vbgf_trunc.cpp") # Compile a C++ templated into a shared object file
 dyn.load(dynlib("vbgf_trunc"))
+
+# Run the model
+obj = MakeADFun(
+  data = list(Length = Obs_i, Age = Age_i, min_size = min_size, max_size = max_size), 
+  parameters = list(Linf = 400, Kappa = .3,T_zero = 0 ,LogSigma = 0), DLL = "vbgf_trunc") # Prepare model object, data, and initial parameter estimates for fitting
+opt1 = nlminb(obj$par, obj$fn, obj$gr) # Fit model using nlminb
+summ = summary(sdreport(obj)) # Get summary of parameter estimates
+summ
+
+# Plot the fit
+curve(summ[1,1] * (1 - exp(-summ[2,1] * (x - summ[3,1]))), from = 0 , to = Max_age, col = 2, add=T)
